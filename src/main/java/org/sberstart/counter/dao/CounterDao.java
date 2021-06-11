25165ae2dccd8fe3bfe5355448f40574065efb3d
kukinpower <thekukinpower@gmail.com>
2021-06-11 13:38:38 +0300

    add counter dao javadoc

--- src/main/java/org/sberstart/counter/dao/CounterDao.java
+++ src/main/java/org/sberstart/counter/dao/CounterDao.java
@@ -5,13 +5,35 @@ import org.sberstart.counter.model.Counter;
 
 public interface CounterDao {
 
+  /**
+   * Find counter object by id
+   * @param id id of a counter
+   * @return counter object, found by id
+   */
   Counter getCounterById(Integer id);
 
+  /**
+   * Add new counter object
+   * @param counter model object to add
+   * @return model added
+   */
   Counter addCounter(Counter counter);
 
+  /**
+   * @return all existing counters list
+   */
   List<Counter> getAllCounters();
 
+  /**
+   * @param id id of a counter
+   * @return counter object, removed from database
+   */
   Counter deleteCounterById(Integer id);
 
+  /**
+   * Increment counter value by one
+   * @param id id of a counter
+   * @return counter object
+   */
   Counter incrementCounterById(Integer id);
 }
